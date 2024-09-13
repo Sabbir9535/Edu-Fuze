f5a4586bd3065c4f63c747553b892da78c1f7431
Sabbir hossen <74649145+Sabbir9535@users.noreply.github.com>
2024-09-13 21:22:07 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('navbar-toggle');
-    const navbarLinks = document.getElementById('navbar-links').querySelector('ul');
-
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active');
-    });
-});
