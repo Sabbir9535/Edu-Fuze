bf7d22ed60bd222eaf225486d358383d87958f72
Sabbir hossen <74649145+Sabbir9535@users.noreply.github.com>
2025-01-05 01:06:29 +0600

    Delete common.js

--- common.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.querySelector('#navbar-toggle');
-    const navLinks = document.querySelector('.nav-link');
-    toggleBtn.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-});
-
-
